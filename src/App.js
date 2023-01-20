ff9e25fa49419af9cdddef5151f98bdbbeca46fe
Ubuntu <srikanthreddy.poco@gmail.com>
2023-01-20 18:48:30 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="App">
       <button className='btn btn-success'>My Button</button>
     </div>
+    </>
   );
 }
 
