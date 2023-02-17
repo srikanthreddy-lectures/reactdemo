5c62643515e3ac73ec71adbe645911fa63acc49c
Ubuntu <srikanthreddy.poco@gmail.com>
2023-02-17 07:04:53 +0000

    setup gh-pages

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import Cookies from "universal-cookie";
 
 function Login() {
 
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [login, setLogin] = useState(false);
@@ -18,7 +19,7 @@ const handleSubmit = (e) => {
 
    const configuration = {
     method: "post",
-    url: "https://cb18-144-24-129-185.in.ngrok.io/user/login",
+    url: "http://localhost:4000/user/login",
     data: {
       email,
       password,
